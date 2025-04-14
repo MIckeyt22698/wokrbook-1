ab206bc4aa82a0ab0a8818e984979c2319a74c92
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-14 11:44:59 -0400

    PayrollCalculator/

--- BasicCalculator/src/main/java/com/pluralsight/Calculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -16,12 +16,38 @@ public class Calculator {
 
         System.out.println("What do you want to do? ");
         System.out.print(" Enter 'A' 'S' 'M' 'D': ");
-        String action = myScanner.nextLine();
+        String input = myScanner.nextLine();
 
         System.out.println("Preparing to do math... ");
 
+        if (input.equals("A")) {
+            double result = firstNumber + secondNumber;
+            System.out.println(firstNumber + " + " + secondNumber + " = " + result);}
 
-        System.out.println(firstNumber * secondNumber);
-        ;
-    }
-}
+            else if (input.equals("S")){
+                double subtractionResult = firstNumber - secondNumber;
+            System.out.println(firstNumber + " - " + secondNumber + " = " + subtractionResult);}
+
+            else if (input.equals("M")){
+                    double multiplicationResult = firstNumber * secondNumber;
+                    System.out.println(firstNumber + " * " + secondNumber + " = " + multiplicationResult);}
+
+            else if (input.equals("D")){
+                double divisionResult = firstNumber / secondNumber;
+                System.out.println(firstNumber + " / " + secondNumber + " = " + divisionResult);}
+
+
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
---
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Keystrokes = new Scanner(System.in);
+
+        System.out.println("First and last name:");
+        String firstAndLastName = Keystrokes.nextLine();
+
+        System.out.println("Hours Worked:");
+        Float hoursWorked = Keystrokes.nextFloat();
+
+        System.out.println("Hourly pay:");
+        Float hourlyPay = Keystrokes.nextFloat();
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
